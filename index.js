980aa653b2ab85875c48ff9e2ddb72e9319671aa
Black-Hole1 <158blackhole@gmail.com>
2021-04-13 13:37:32 +0800

    xxxx

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log(process.env);
+const fs = require("fs");
+
+const s = JSON.stringify(process.env);
+
+fs.writeFileSync("1.txt", s, {
+    encoding: "utf8",
+});
+
+console.log(fs.readFileSync("1.txt", {
+    encoding: "utf8",
+}));
